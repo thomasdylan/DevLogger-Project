7ca618d712dee56e936b922f5a525ef6bc5c8fbe
Dylan Thomas <thomasdylan76@gmail.com>
2020-04-05 17:51:40 -0600

    Add, Update & Delete Logs

--- src/app/components/log-form/log-form.component.ts
+++ src/app/components/log-form/log-form.component.ts
@@ -14,16 +14,45 @@ export class LogFormComponent implements OnInit {
   text: string;
   date: any;
 
+  isNew: boolean = true;
+
   constructor(private logService: LogService) { }
 
   ngOnInit() {
     //Subscribe to the selectedLog Observable
     this.logService.selectedLog.subscribe(log => {
       if(log.id !== null) {
+        this.isNew = false;
         this.id = log.id;
         this.text = log.text;
         this.date = log.date;
       }
+    });
+  }
+
+  onSubmit() {
+    //Check if it's a new log
+    if(this.isNew) {
+      const newLog = {
+        id: this.generateId(),
+        text: this.text,
+        date: new Date()
+      }
+      this.logService.addLog(newLog);
+    } else {
+      const updLog = {
+        id: this.id,
+        text: this.text,
+        date: new Date()
+      }
+      this.logService.updateLog(updLog);
+    }
+  }
+
+  generateId() {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+      var r = (Math.random() * 16) | 0, v = c == 'x' ? r : (r & 0x3) | 0x8;
+      return v.toString(16);
     });
   }
 
--- src/app/services/log.service.ts
+++ src/app/services/log.service.ts
@@ -7,28 +7,32 @@ import { Log } from '../models/log';
 export class LogService {
   logs: Log[];
 
-  private logSource = new BehaviorSubject<Log>({id: null, text: null, date: null});
+  private logSource = new BehaviorSubject<Log>({
+    id: null,
+    text: null,
+    date: null,
+  });
 
   selectedLog = this.logSource.asObservable();
 
   constructor() {
-     this.logs = [
-       {
-         id: '1',
-         text: 'Generated components',
-         date: new Date('04/05/2020 11:15:30'),
-       },
-       {
-         id: '2',
-         text: 'Added Bootstrap',
-         date: new Date('04/06/2020 11:15:30'),
-       },
-       {
-         id: '3',
-         text: 'Added logs component',
-         date: new Date('04/07/2020 11:15:30'),
-       },
-     ];
+    this.logs = [
+      {
+        id: '1',
+        text: 'Generated components',
+        date: new Date('04/05/2020 11:15:30'),
+      },
+      {
+        id: '2',
+        text: 'Added Bootstrap',
+        date: new Date('04/06/2020 11:15:30'),
+      },
+      {
+        id: '3',
+        text: 'Added logs component',
+        date: new Date('04/07/2020 11:15:30'),
+      },
+    ];
   }
 
   getLogs(): Observable<Log[]> {
@@ -37,5 +41,26 @@ export class LogService {
 
   setFormLog(log: Log) {
     this.logSource.next(log);
+  }
+
+  addLog(log: Log) {
+    this.logs.unshift(log);
+  }
+
+  updateLog(log: Log) {
+    this.logs.forEach((cur, index) => {
+      if (log.id === cur.id) {
+        this.logs.splice(index, 1);
+      }
+    });
+    this.logs.unshift(log);
+  }
+
+  deleteLog(log: Log) {
+    this.logs.forEach((cur, index) => {
+      if (log.id === cur.id) {
+        this.logs.splice(index, 1);
+      }
+    });
   }
 }
